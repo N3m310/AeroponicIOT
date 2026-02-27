a015155c5cd3876d8dce6ceb86082876ac473598
N3m310 <namtranquoc310@gmail.com>
2026-02-27 09:50:36 +0700

    massive change

--- obj/Debug/net10.0/AeroponicIOT.AssemblyInfo.cs
+++ obj/Debug/net10.0/AeroponicIOT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroponicIOT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844a8e35641ded53edbc0ee2f0dfb2812e5c7f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb05277efe5de74a3398a34fac19e214df3c2ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroponicIOT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroponicIOT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
